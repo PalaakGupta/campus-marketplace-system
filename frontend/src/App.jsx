cb0912a0c8950bea72ad60a7237659941790f047
Daya Kumari <dayalbagh2001@gmail.com>
2026-06-02 09:19:22 +0530

    Initial  setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Login from './pages/Login/Login';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
